5f47166855e58d2e0e08e0aefefd770c7a11e6af
Thomas Pickup <tpickup01@qub.ac.uk>
2017-11-15 20:58:53 +0000

    Project created

---
+++ Projects/Weathered/Weathered/Weathered/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Weathered
+//
+//  Created by Thomas Pickup on 15/11/2017.
+//  Copyright © 2017 Thomas Pickup. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Projects/Weathered/Weathered/Weathered/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Weathered
+//
+//  Created by Thomas Pickup on 15/11/2017.
+//  Copyright © 2017 Thomas Pickup. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
